94d9115574af722a60f2ac7f39945056fbfd832f
Techalone <techalone2@gmail.com>
2025-02-16 04:56:43 +1245

    5th commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import router from './router/index.js';  // Import the router
+import router from './router/index.ts';  // Import the router
 
 createApp(App)
   .use(router)  // Use the router
